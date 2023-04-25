daeb6608eba0938670be364b66c7fa4a5dc16ee5
Jason Smallwood <jason.l.smallwood@gmail.com>
2023-04-25 14:32:35 -0400

    Update sqlserveringestcontrol-initialize.sql

--- azureoptimizationengine/model/sqlserveringestcontrol-initialize.sql
+++ azureoptimizationengine/model/sqlserveringestcontrol-initialize.sql
@@ -4,3 +4,17 @@ BEGIN
     VALUES 
         ('recommendationsexports', '1901-01-01T00:00:00Z', -1, 'Recommendations')
 END
+
+IF NOT EXISTS (SELECT * FROM [dbo].[SqlServerIngestControl] WHERE StorageContainerName = 'consumptionactualexports')
+BEGIN
+    INSERT INTO [dbo].[SqlServerIngestControl] 
+    VALUES 
+        ('consumptionactualexports', '1901-01-01T00:00:00Z', -1, 'ConsumptionActual')
+END
+
+IF NOT EXISTS (SELECT * FROM [dbo].[SqlServerIngestControl] WHERE StorageContainerName = 'consumptionamortizedexports')
+BEGIN
+    INSERT INTO [dbo].[SqlServerIngestControl] 
+    VALUES 
+        ('consumptionamortizedexports', '1901-01-01T00:00:00Z', -1, 'ConsumptionAmortized')
+END
